9614ec8afd6aa9d69a360d92eac46cabe890c281
Ramkishor H <ramhosamane@gmail.com>
2020-11-30 17:40:00 +0530

    added photo taker

--- FoodApp/camera.py
+++ FoodApp/camera.py
@@ -4,6 +4,8 @@ from imutils.video import WebcamVideoStream
 
 
 class VideoCamera(object):
+    i = 200
+    fruit = "orange"
     def __init__(self):
         self.stream = WebcamVideoStream(src=0).start()
 
@@ -12,14 +14,18 @@ class VideoCamera(object):
 
     def get_frame(self):
         x,y = 50,50
-        w,h = 150,150
+        w,h = 200,200
         image = self.stream.read()
         crop_img = image[x:x+w,y:y+h ]
+        #cv2.imwrite('static/people_photo/'+self.fruit+"_"+str(self.i)+'.jpg',crop_img)
+        cv2.imwrite('static/people_photo/fruit.jpg',crop_img)
+        
+        self.i+=1
         cv2.rectangle(image, (x, y), (x+w, y+h), (0,255,0), 2)
+
 
         image = cv2.flip(image, 1)
         #ret,crop_img = cv2.imencode('.jpg',crop_img)
-        cv2.imwrite('static/people_photo/fruit.jpg',crop_img)
 
         ret,jpeg = cv2.imencode('.jpg',image)
 
---
+++ Photo Taker/phototaker.py
@@ -0,0 +1,34 @@
+import cv2
+import os
+
+try:
+    os.mkdir("Output")
+except FileExistsError:
+    print("Directory already exists")
+
+output_path = "Output/"
+fruit = input("Enter fruit Name : ")
+no_of_shots = 0
+max_shots = int(input("Enter No of shots : "))
+
+
+camera = cv2.VideoCapture(0)
+x,y = 50,50
+w,h = 200,200
+
+
+while no_of_shots < max_shots:
+    ret,image = camera.read()
+    crop_img = image[x:x+w,y:y+h ]
+    cv2.imwrite(output_path+fruit+"_"+str(no_of_shots)+'.jpg',crop_img)        
+    cv2.rectangle(image, (x, y), (x+w, y+h), (0,255,0), 2)
+    image = cv2.flip(image,1)
+    cv2.imshow("Camera",image)
+
+
+    cv2.waitKey(500)
+    no_of_shots+=1
+
+
+camera.release()
+cv2.destroyAllWindows()
